9ee80e7609c441a90c8aad3b617ca357c6026901
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-06-28 14:59:23 +0300

    Add project files.

---
+++ Lab2ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex 1: Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+             citit de la tastatura.*/
+
+            Console.WriteLine("ACEST PROGRAM VA CALCULA SUMA CIFRELOR UNUI NUMAR");
+
+            Console.WriteLine("Introduceti n ");
+            int n = int.Parse(Console.ReadLine());
+
+            int suma = 0;
+            while (n != 0) 
+            {
+                int cifra = n % 10; //extragem ultima cifra
+                suma += cifra; //adaugam vifra la suma totala
+                n /= 10; //eliminam ultima cifra prin impartirea la 10
+            }
+            Console.WriteLine("Suma cifrelor este " +suma);
+        }
+    }
+}
